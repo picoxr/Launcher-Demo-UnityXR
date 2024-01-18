104c1e2f4e73da37b64a0e0370bcc4b0fc39bf75
jesus <jesusatorrasagasti@bytedance.com>
2024-01-18 17:54:54 +0100

    New Unity 2022 project

---
+++ Assets/Scripts/LauncherUtils.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.PXR;
+using UnityEngine;
+
+public class LauncherUtils : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PXR_Enterprise.InitEnterpriseService();
+        PXR_Enterprise.BindEnterpriseService();
+    }
+
+    public void OpenWifiSettingsPanel()
+    {
+        PXR_Enterprise.StartVrSettingsItem(StartVRSettingsEnum.START_VR_SETTINGS_ITEM_WIFI, true, 0);
+    }
+
+    public void LaunchApp()
+    {
+        //PXR_Enterprise.StartActivity();
+    }
+
+
+    //This is a protected API. You need to add <meta-data android:name="pico_advance_interface" android:value="0"/> to the app's AndroidManifest.xml file for calling this API, after which the app is unable to be published on the PICO Store.
+    public void Shutdown()
+    {
+        PXR_Enterprise.ControlSetDeviceAction(DeviceControlEnum.DEVICE_CONTROL_SHUTDOWN, (result) => Debug.Log("Reboot failed: " + result));
+    }
+
+    //This is a protected API. You need to add <meta-data android:name="pico_advance_interface" android:value="0"/> to the app's AndroidManifest.xml file for calling this API, after which the app is unable to be published on the PICO Store.
+    public void Reboot()
+    {
+        PXR_Enterprise.ControlSetDeviceAction(DeviceControlEnum.DEVICE_CONTROL_REBOOT, (result) => Debug.Log("Reboot failed: " + result));
+    }
+
+    public void SetAsLauncherApp()
+    {
+        PXR_Enterprise.SetAPPAsHome(SwitchEnum.S_ON, "com.PICO.LauncherDemo");
+    }
+
+    public void ResetLauncherApp()
+    {
+        PXR_Enterprise.SetAPPAsHome(SwitchEnum.S_OFF, "com.PICO.LauncherDemo");
+    }
+    
+}
