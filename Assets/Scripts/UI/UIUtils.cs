40af36b1ad16f72f17bf1d4a7534edcb321942e0
jesus <jesusatorrasagasti@bytedance.com>
2024-01-19 14:49:38 +0100

    Fixes

--- Assets/Scripts/UI/UIUtils.cs
+++ Assets/Scripts/UI/UIUtils.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class UI_Back : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        XRSettings.eyeTextureResolutionScale = 1.3f;
     }
 
     // Update is called once per frame
